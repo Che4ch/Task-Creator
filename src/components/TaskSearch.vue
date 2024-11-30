234b1598ccd3c2c73d02e5928e6fd12d0fc95f3d
Fedor Susanin <fsusanin@psystems.biz>
2024-11-30 22:22:47 +0300

    feat(baseLayout): Created base project layout (UI-2)

---
+++ src/components/TaskSearch.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-text-field
+    placeholder="Search"
+    prepend-inner-icon="mdi-magnify"
+    hide-details
+    class="mr-2"
+  />
+</template>
+
+<style scoped lang="scss"></style>
